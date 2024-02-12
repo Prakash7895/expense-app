01805177c503c409f4e85f0809bf93167ad9dd30
Prakash7895 <prakashchoudhary0141@gmail.com>
2024-02-12 09:40:33 +0530

    bug fix

--- server/src/controller/user.ts
+++ server/src/controller/user.ts
@@ -84,6 +84,7 @@ export const userLogin = async (req: Request, res: Response) => {
         status: 'success',
         message: 'User logged in successfully.',
         token,
+        headers: req.headers,
         user: {
           email: user.email,
           phone: user.phone,
